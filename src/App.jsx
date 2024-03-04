a336ddfb6ba683b30e7caf1aa173e30b6cba5c27
iamvinay79 <93002755+iamvinay79@users.noreply.github.com>
2024-03-04 21:44:03 +0530

    second

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Header from "./assets/components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="bg-indigo-600 w-full h-screen"></div>
+    </div>
+  );
+}
+
+export default App;
